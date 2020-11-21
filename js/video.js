9afc9acb4f89112ff2bf01d32753262db39fd7d3
negustaf <negustaf@umich.edu>
2020-11-20 19:59:38 -0500

    commit 2

--- js/video.js
+++ js/video.js
@@ -9,15 +9,19 @@ document.querySelector("#play").addEventListener("click", function() {
 document.querySelector("#pause").addEventListener("click", function() {
 	video.pause();
 });
+/* SLOW DOWN VIDEO BUTTON */
 slower.onclick = function setPlaySpeed() {
   video.playbackRate -= 0.1;
 };
+/* SPEED UP VIDEO BUTTON */
 faster.onclick = function setPlaySpeed() {
   video.playbackRate += 0.1;
 };
+/* SKIP 5 SECONDS VIDEO BUTTON */
 skip.onclick = function currentTime() {
 	video.currentTime += 5;
 };
+/* MUTE TOGGLE VIDEO BUTTON */
 mute.onclick = function() {
 	if (video.muted) {
 		video.muted = false;
@@ -28,6 +32,7 @@ mute.onclick = function() {
 		mute.innerHTML = 'unmute';
 	}
 };
+/* VOLUME SLIDER VIDEO BUTTON */
 var slider = document.getElementById('volumeSlider');
 var output = document.getElementById('volume');
 output.innerHTML = slider.value;
@@ -39,3 +44,13 @@ var setVolume = function(){
 };
 slider.addEventListener('change',setVolume);
 slider.addEventListener('input',setVolume);
+
+/* ADD GREYSCALE (.oldTime) CLASS TO VIDEO */
+old.onclick = function() {
+	video.classList.add('oldTime');
+}
+original.onclick = function() {
+	video.classList.remove('oldTime')
+}
+
+/* HOW DO YOU ORGANIZE CODE IN JS LANGUAGE? */
